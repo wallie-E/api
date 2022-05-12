c86202e2046316d6d4efc48aeb0095e156b7739c
chenfengqi <chenfengqi_i@didiglobal.com>
2022-05-12 21:53:43 +0800

    修改为数据库为本地

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect('mongodb://localhost:27017/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
